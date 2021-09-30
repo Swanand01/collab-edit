d77dd539958dc1d24c683f7dfb076b2b4c5d8628
Swanand01 <swanandmathekar@gmail.com>
2021-09-30 12:56:04 +0530

    changed the name

--- collab-code/asgi.py
+++ collab-code/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fileshare project.
+ASGI config for collab-code project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fileshare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collab-code.settings')
 
 application = get_asgi_application()
--- collab-code/wsgi.py
+++ collab-code/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fileshare project.
+WSGI config for collab-code project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fileshare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collab-code.settings')
 
 application = get_wsgi_application()
